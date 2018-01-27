c351e81153a297b9e7e9345da6db66f6f928699a
Joe Wong <jjwong05@gmail.com>
2018-01-27 02:15:27 -0500

    Update config file

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function (config) {
 		autoWatch: false,
 		singleRun: true,
 		preprocessors: {
-			'js/*.js': [ 'coverage' ]
+			'js/toolbar.js': [ 'coverage' ]
 		},
 		reporters: [ 'dots', 'coverage' ],
 		coverageReporter: {
