5b0ff4ab4edfa9c8781532f14de9267119c68ea6
Joe Wong <jjwong05@gmail.com>
2018-01-27 02:00:20 -0500

    Add node coverage dependencies

---
+++ karma.conf.js
@@ -0,0 +1,31 @@
+/* eslint-env node */
+module.exports = function (config) {
+	config.set({
+		// To debug, run `npm run karma-debug` and press the "Debug" button in the browser window
+		browsers: [ 'ChromeHeadless' ],
+		frameworks: [ 'qunit' ],
+		files: [
+			'js/toolbar.js',
+			'test/toolbar.test.js'
+		],
+		autoWatch: false,
+		singleRun: true,
+		preprocessors: {
+			'js/*.js': [ 'coverage' ]
+		},
+		reporters: [ 'dots', 'coverage' ],
+		coverageReporter: {
+			reporters: [
+				{ type: 'text-summary' },
+				{ type: 'html', dir:'coverage/' },
+				{ type: 'lcovonly', dir: 'coverage/' },
+			],
+			check: { global: {
+				functions: 90,
+				statements: 50,
+				branches: 50,
+				lines: 50
+			} }
+		}
+	});
+};
