85de165bb554f555f842a3c8e56a663b119bb1bf
Joe Wong <jjwong05@gmail.com>
2018-01-27 02:20:07 -0500

    Ignore min files

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function (config) {
 		autoWatch: false,
 		singleRun: true,
 		preprocessors: {
-			'js/toolbar.js': [ 'coverage' ]
+			'js/*.js': [ 'coverage' ]
 		},
 		reporters: [ 'dots', 'coverage' ],
 		coverageReporter: {
