c6fee98891cc3c1d8cf27700462c70f34381c5ad
Joe Wong <jjwong05@gmail.com>
2022-11-27 10:10:14 -0500

    Test

--- karma.conf.js
+++ karma.conf.js
@@ -20,6 +20,7 @@ module.exports = function (config) {
 			reporters: [
 				{ type: 'text-summary' },
 				{ type: 'html', dir:'coverage/' },
+				{ type: 'lcov', dir:'coverage/' },
 				{ type: 'lcovonly', dir: 'coverage/' },
 			],
 			check: { global: {
